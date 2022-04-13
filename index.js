02f83a8cc7293bf8e82575c0a9d57fbf4eb4b961
Ramiro Michat <ramamichat@gmail.com>
2022-04-13 15:14:06 -0300

    Adds a comment on index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-// futuras implementaciones:
+// future implementations/futuras implementaciones:
 // scroll-up button que aparece cuando deslizas hacia abajo.
 // animaciones y fade-ins al scrollear.
